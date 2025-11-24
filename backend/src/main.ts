c4c05be24c1a4a27e399de8521d410bc798532c6
nqh261004 <ngquyhung2610@gmail.com>
2025-11-24 12:10:35 +0700

    Hoàn thành 100%

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -9,7 +9,7 @@ async function bootstrap() {
   }));
 
   app.enableCors({
-    origin: 'http://localhost:5173', 
+    origin: ['http://localhost:5173', 'http://localhost:8080'], 
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', 
     credentials: true, 
   });
